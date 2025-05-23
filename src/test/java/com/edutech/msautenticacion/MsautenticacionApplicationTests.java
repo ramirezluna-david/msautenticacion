d162743c2e21534e4b439291ab75593fd352530f
David A. Ramírez Luna <davidrrluna@gmail.com>
2025-05-23 18:02:23 -0400

    first commit

---
+++ src/test/java/com/edutech/msautenticacion/MsautenticacionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edutech.msautenticacion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsautenticacionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
